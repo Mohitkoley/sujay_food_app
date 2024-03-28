fa2ace3d6143496d8a8c347f9a6e531036b5b02b
Mohit <mohitkoley5@gmail.com>
2024-03-28 16:09:01 +0530

    added splash screen

--- android/app/src/main/kotlin/com/example/food_deliver/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/food_deliver/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.food_deliver
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
-class MainActivity: FlutterActivity()
+
+class MainActivity: FlutterActivity() {
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
+}
